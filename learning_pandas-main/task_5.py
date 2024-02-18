a1c2f0dc9d30eb1c4965a65ad94d4d4e156ad404
KLOHTORM <klohtorm@gmail.com>
2024-02-18 23:07:03 +0300

    Сделан 1 пункт в задании 5

---
+++ learning_pandas-main/task_5.py
@@ -0,0 +1,25 @@
+"""
+Написать EDA для датасета про пингвинов
+Необходимо:
+1. Использовать 2-3 точечных графика
+2. Применить доп измерение в точечных графиках, используя
+аргументы hue, size, stile
+3. Использовать PairGrid с типом графика на ваш выбор
+4. Изобразить Heatmap
+5. Использовать 2-3 гистограммы
+
+Чтобы подключить датасет с
+пингвинами, воспользуйтесь данным
+скриптом:
+penguins = sns.load_dataset("penguins")
+penguins.head()
+"""
+
+from seaborn import  load_dataset, scatterplot
+from matplotlib.pyplot import show
+data = load_dataset('penguins')
+
+scatterplot(data=data, x='flipper_length_mm', y='body_mass_g')
+
+
+show()
